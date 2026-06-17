b0cd016b8843a1497d241ba274ee5db321e8e74e
suniilghate <sunil.82ghate@gmail.com>
2026-06-17 22:00:39 +0530

    Adding and applying css stylesheet to html/react element

--- chapter-1/App.js
+++ chapter-1/App.js
@@ -1,3 +1,5 @@
-const elemHeading = React.createElement("h1", {}, "Namaste React Hello World using React");
+const elemHeading = React.createElement("h1", {
+    "id": "headingId",
+}, "Namaste React Hello World using React");
 const doomRoot = ReactDOM.createRoot(document.getElementById("root"));
 doomRoot.render(elemHeading);
